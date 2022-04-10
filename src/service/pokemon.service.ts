e2b037c33e2b08fa06e2fb2ca9479a6c3c8379b9
shahoul95 <66682178+shahoul95@users.noreply.github.com>
2022-04-10 17:51:07 +0200

    rename url endpoint api

--- src/service/pokemon.service.ts
+++ src/service/pokemon.service.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import PokemonDataType from "../type/pokemon.type"
 class PokemonDataService {
     getPokemon(offset:number,limit:number) {
-        return axios.get<PokemonDataType>(`http://localhost:8080/pokedata/pokemon/${offset}/${limit}`);
+        return axios.get<PokemonDataType>(`http://localhost:8080/pokemon/${offset}/${limit}`);
     }
     getPokemonByUrl(url : string){
         return axios.get<PokemonDataType>(url);
